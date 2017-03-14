7216d6b9d203e1b039862b26c3a22d1ce529b943
unknown <>
2017-03-14 18:03:34 +0800

    init

--- js/hr.js
+++ js/hr.js
@@ -1,4 +1,4 @@
-var t,m,p,q;
+var t,m,p,q,time;
 $(document).ready(function(){
 	                var dialog_box_2=document.getElementById("dialog_box_2").getElementsByTagName("img");
 	                var programmer_2=document.getElementById("page2").getElementsByClassName("programmer");
@@ -6,7 +6,19 @@ $(document).ready(function(){
 	                var programmer_3=document.getElementById("page3").getElementsByClassName("programmer");
 	                var dialog_box_4=document.getElementById("dialog_box_4").getElementsByTagName("img");
 	                var programmer_4=document.getElementById("page4").getElementsByClassName("programmer");
+					var num=0;
+					function type(s,obj){	  
+					    time=setInterval(function(){
+					    obj.append(s.charAt(num));
+					    if(num === s.length){
+					    clearInterval(time);
+					    num = 0;
+					    }
+					    num++;
+					  },50);
+					};
 					function talk_2(){
+						$('#fp-nav ').hide();
 						$(".cover").show();
 						$('#dialog_box_2').show();
 						$.fn.fullpage.setAllowScrolling(false);
@@ -23,48 +35,16 @@ $(document).ready(function(){
 								$("#dialog_box_2").empty().append(img);
 								switch(j){
 									case 0:
-	                                	$('#dialog_box_2').typetype('0开发及全校师生的电脑维护等工作。UED部门全面负责所有面向用户的开发和设计0开发及全校师生的电脑维护等工作。UED部门全面负责所有面向用户的开发和设计0开发及全校师生的电脑维护等工作。UED部门全面负责所有面向用户的开发和设计0开发及全校师生的电脑维护等工作。UED部门全面负责所有面向用户的开发和设计',{
-									    	e: 0,  //error rate. (use e=0 for perfect typing)
-									    	//t: 100,  interval between keypresses
-									    	callback:function(){
-									    		$('.cover').hide();
-									    		$('#dialog_box_2').hide();
-									    		$.fn.fullpage.setAllowScrolling(true);
-									    	}
-									    });
+	                                	type('0开发及全校师生的电脑维护等工作。UED部门全面负责所有面向用户的开发和设计0开发及全校师生的电脑维护等工作。UED部门全面负责所有面向用户的开发和设计',$('#dialog_box_2'));
 									break;
 									case 1:
-									    $('#dialog_box_2').typetype('1开发及全校师生的电脑维护等工作。UED部门全面负责所有面向用户的开发和设计',{
-									    	e: 0,  //error rate. (use e=0 for perfect typing)
-									    	//t: 100,  interval between keypresses
-									    	callback:function(){
-									    		$('.cover').hide();
-									    		$('#dialog_box_2').hide();
-									    		$.fn.fullpage.setAllowScrolling(true);
-									    	}
-									    });
+									    type('产品部隶属于日新网最酷的技术中心，主要负责日新互联网产品的运营、市场推广，我们也致力交大的官网建设，不断便利交大师生的学习与生活。同时负责技术中心的行政事务，对接日常的活动和对外交流。如果你有热情，有想法，有责任，日新技术产品部欢迎你。',$('#dialog_box_2'));
 									break;
 									case 2:
-									    $('#dialog_box_2').typetype('2开发及全校师生的电脑维护等工作。UED部门全面负责所有面向用户的开发和设计',{
-									    	e: 0,  //error rate. (use e=0 for perfect typing)
-									    	//t: 100,  interval between keypresses
-									    	callback:function(){
-									    		$('.cover').hide();
-									    		$('#dialog_box_2').hide();
-									    		$.fn.fullpage.setAllowScrolling(true);
-									    	}
-									    });
+									    type('你喜欢前端吗？前端的世界一直在变化着，在各种熟悉的语言进化中迅速的化学反应。也许你和我们一样，对前端的理解也在不断刷新。我们犹如一群站在互联网风口上的攻城狮，眼前的空间充满了未知，需要我们去开拓去开垦，需要我们逆风而上，不为别的，只为能在这虚拟与现实交错的世界里昂首飞翔！    日新数据开发部门主要是做后台数据开发及日新网服务器维护，当前端的页面做好之后，我们所需要做的就是将后台数据内容渲染到页面中，使页面有了动态的内容。在这里你可以学习到php或者是python的知识，也能学习如何操作服务器。既能学到课堂学不到的知识，又能充实自己的课余生活。',$('#dialog_box_2'));
 									break;
 									case 3:
-									    $('#dialog_box_2').typetype('3开发及全校师生的电脑维护等工作。UED部门全面负责所有面向用户的开发和设计',{
-									    	e: 0,  //error rate. (use e=0 for perfect typing)
-									    	//t: 100,  interval between keypresses
-									    	callback:function(){
-									    		$('.cover').hide();
-									    		$('#dialog_box_2').hide();
-									    		$.fn.fullpage.setAllowScrolling(true);
-									    	}
-									    });
+									    type('0开发及全校师生的电脑维护等工作。UED部门全面负责所有面向用户的开发和设计0开发及全校师生的电脑维护等工作。UED部门全面负责所有面向用户的开发和设计',$('#dialog_box_2'));
 									break;
 									default:
 									break;
@@ -89,26 +69,21 @@ $(document).ready(function(){
 								$("#dialog_box_3").empty().append(img);
 								switch(j){
 									case 0:
-	                                	$('#dialog_box_3').typetype('0开发及全校师生的电脑维护等工作。UED部门全面负责所有面向用户的开发和设计',{
-									    	e: 0,  //error rate. (use e=0 for perfect typing)
-									    	//t: 100,  interval between keypresses
-									    	callback:function(){
-									    		$('.cover').hide();
-									    		$('#dialog_box_3').hide();
-									    		$.fn.fullpage.setAllowScrolling(true);
-									    	}
-									    });
+	                                	type('0开发及全校师生的电脑维护等工作。UED部门全面负责所有面向用户的开发和设计0开发及全校师生的电脑维护等工作。UED部门全面负责所有面向用户的开发和设计',$('#dialog_box_3'));
 									break;
 									case 1:
-									    $('#dialog_box_3').typetype('1开发及全校师生的电脑维护等工作。UED部门全面负责所有面向用户的开发和设计',{
-									    	e: 0,  //error rate. (use e=0 for perfect typing)
-									    	//t: 100,  interval between keypresses
-									    	callback:function(){
-									    		$('.cover').hide();
-									    		$('#dialog_box_3').hide();
-									    		$.fn.fullpage.setAllowScrolling(true);
-									    	}
-									    });
+									    type('0开发及全校师生的电脑维护等工作。UED部门全面负责所有面向用户的开发和设计0开发及全校师生的电脑维护等工作。UED部门全面负责所有面向用户的开发和设计',$('#dialog_box_3'));
+									break;
+									case 2:
+									    type('0开发及全校师生的电脑维护等工作。UED部门全面负责所有面向用户的开发和设计0开发及全校师生的电脑维护等工作。UED部门全面负责所有面向用户的开发和设计',$('#dialog_box_3'));
+									break;
+									case 3:
+									    
+									    type('0开发及全校师生的电脑维护等工作。UED部门全面负责所有面向用户的开发和设计0开发及全校师生的电脑维护等工作。UED部门全面负责所有面向用户的开发和设计',$('#dialog_box_3'));
+									    
+									break;
+									case 4:
+									    type('0开发及全校师生的电脑维护等工作。UED部门全面负责所有面向用户的开发和设计0开发及全校师生的电脑维护等工作。UED部门全面负责所有面向用户的开发和设计',$('#dialog_box_3'));
 									break;
 									default:
 									break;
@@ -133,37 +108,19 @@ $(document).ready(function(){
 								$("#dialog_box_4").empty().append(img);
 								switch(j){
 									case 0:
-	                                	$('#dialog_box_4').typetype('0开发及全校师生的电脑维护等工作。UED部门全面负责所有面向用户的开发和设计',{
-									    	e: 0,  //error rate. (use e=0 for perfect typing)
-									    	//t: 100,  interval between keypresses
-									    	callback:function(){
-									    		$('.cover').hide();
-									    		$('#dialog_box_4').hide();
-									    		$.fn.fullpage.setAllowScrolling(true);
-									    	}
-									    });
+	                                	type('0开发及全校师生的电脑维护等工作。UED部门全面负责所有面向用户的开发和设计0开发及全校师生的电脑维护等工作。UED部门全面负责所有面向用户的开发和设计',$('#dialog_box_4'));
 									break;
 									case 1:
-									    $('#dialog_box_4').typetype('1开发及全校师生的电脑维护等工作。UED部门全面负责所有面向用户的开发和设计',{
-									    	e: 0,  //error rate. (use e=0 for perfect typing)
-									    	//t: 100,  interval between keypresses
-									    	callback:function(){
-									    		$('.cover').hide();
-									    		$('#dialog_box_4').hide();
-									    		$.fn.fullpage.setAllowScrolling(true);
-									    	}
-									    });
+									    type('0开发及全校师生的电脑维护等工作。UED部门全面负责所有面向用户的开发和设计0开发及全校师生的电脑维护等工作。UED部门全面负责所有面向用户的开发和设计',$('#dialog_box_4'));
 									break;
 									case 2:
-									    $('#dialog_box_4').typetype('2开发及全校师生的电脑维护等工作。UED部门全面负责所有面向用户的开发和设计',{
-									    	e: 0,  //error rate. (use e=0 for perfect typing)
-									    	//t: 100,  interval between keypresses
-									    	callback:function(){
-									    		$('.cover').hide();
-									    		$('#dialog_box_4').hide();
-									    		$.fn.fullpage.setAllowScrolling(true);
-									    	}
-									    });
+									    type('0开发及全校师生的电脑维护等工作。UED部门全面负责所有面向用户的开发和设计0开发及全校师生的电脑维护等工作。UED部门全面负责所有面向用户的开发和设计',$('#dialog_box_4'));
+									break;
+									case 3:
+									    type('0开发及全校师生的电脑维护等工作。UED部门全面负责所有面向用户的开发和设计0开发及全校师生的电脑维护等工作。UED部门全面负责所有面向用户的开发和设计',$('#dialog_box_4'));
+									break;
+									case 4:
+									    type('0开发及全校师生的电脑维护等工作。UED部门全面负责所有面向用户的开发和设计0开发及全校师生的电脑维护等工作。UED部门全面负责所有面向用户的开发和设计',$('#dialog_box_4'));
 									break;
 									default:
 									break;
@@ -183,78 +140,46 @@ $(document).ready(function(){
 				$(".menu").click(function(){
 					$(".cover").show();
 					$(".forms").show();
+					$('#fp-nav ').hide();
 					$.fn.fullpage.setAllowScrolling(false);
 				})//报名表打开
 				$(".close").click(function(){
 					$(".cover").hide();
 					$(".forms").hide();
+					$('#fp-nav ').show();
 					$.fn.fullpage.setAllowScrolling(true);
 				})//关闭报名表
-	$('#fullpage').fullpage({
-		loopBottom:true,
-		verticalCentered:false,
-		afterLoad:function(link,index){
-			switch(index){
-				case 1:
-				function flowermove1(){
-					$('#flower-1').hide();
-					$('#flower-2').show();
-					t=setTimeout(flowermove2,800)
-				}
-				function flowermove2(){
-					$('#flower-1').show();
-					$('#flower-2').hide();
-					m=setTimeout(flowermove1,800);
-				}
-				flowermove1();//草动画
-				function smoke1(){
-					$('#smoke_1').animate({opacity:'0'},500);
-					$('#smoke_2').animate({opacity:'1'},500);
-					p=setTimeout(smoke2,400)
-				}
-				function smoke2(){
-					$('#smoke_1').animate({opacity:'1'},500);
-					$('#smoke_2').animate({opacity:'0'},500);
-					q=setTimeout(smoke1,400)
-				}
-				smoke1();//烟动画
-					$('#train').animate({left:'150px'},2000);//火车动画			       
-				break;
-				case 2:
-				$("#more").fadeIn();
-				break;
-				case 3:
-				$(".gai").addClass("gais");
-				setTimeout(function(){
-					$("#sheji").animate({top:'-64px',opacity:'1'},500);
-				},500)	;			
-				break;
-				case 4:
-				$("#s_dialog_box").animate({opacity:'1',top:'-5'},500);
-				break;
-				default:
-				break;			}
-		},
-		onLeave:function(link,index){
-			switch(index){
-				case 1:
-				clearTimeout(t);clearTimeout(m);clearTimeout(p);clearTimeout(q);
-                $('#train').css("left","450px");
-				break;
-				case 2:
-				$("#more").fadeOut();
-				break;
-				case 3:
-				$(".gai").removeClass("gais");
-                $("#sheji").animate({top:'0px',opacity:'0'},1);
-				break;
-				case 4:
-				$("#s_dialog_box").animate({opacity:'0',top:'5'},1);
-				break;
-				default:
-				break;			}
-		},
-	});
+				$(".cover").click(function(){
+					$(".cover").hide();
+					$(".forms").hide();
+					$('#fp-nav ').show();
+					$('.dialog_box').hide();
+					clearInterval(time);
+					num=0;
+					$.fn.fullpage.setAllowScrolling(true);
+				})//点击遮罩层关闭对话框
+	var firstli=document.getElementById("fp-nav").getElementsByTagName("ul");
+    firstli[0].firstChild.style.display='none';//消除首个导航
+    function  addoption(center,title){
+    	center.change(function(){
+    		title.empty();
+    	if(center.val()=="新闻出版中心"){
+    		title.append("<option value='编辑部'>编辑部</option><option value='记者团'>记者团</option>");
+    	}else if (center.val()=="技术研发中心") {
+            title.append("<option value='网建部'>网建部</option><option value='移动开发部'>移动开发部</option><option value='产品部'>产品部</option><option value='设计部'>设计部</option>");
+    	}else if (center.val()=="行政管理中心") {
+            title.append("<option value='办公室'>办公室</option><option value='外联部'>外联部</option>");
+    	}else if (center.val()=="产品运营中心") {
+            title.append("<option value='日新微博'>日新微博</option><option value='日新影视工作室'>日新影视工作室</option><option value='视觉摄影产品'>视觉摄影产品</option><option value='日新青梅竹马'>日新青梅竹马</option><option value='《交大青年》杂志社'>《交大青年》杂志社</option>");
+        }
+    	});
+    }
+    addEventListener('load',addoption($("#join_center1"),$("#join_title1")));
+    addEventListener('load',addoption($("#join_center2"),$("#join_title2")));
+    addEventListener('load',addoption($("#join_center3"),$("#join_title3")));
+
+
+
 	var submit = $(".buttons");
 	submit.bind("click",function(){
 	    //event.preventDault 兼容ie&&ff
@@ -272,9 +197,12 @@ $(document).ready(function(){
 	    sex_input = parent.find("input[name='user_sex']"),
 	    name_input = parent.find("input[name='user_name']"),
 	    phone_input = parent.find("input[name='user_phone']"),
+	    QQ_input = parent.find("input[name='user_QQ']"),
 	    college_input = parent.find("select[name='user_college']"),
-	    grade_input = parent.find("select[name='user_grade']"),
+	    grade_input = parent.find("input[name='user_grade']"),
 	    major_input = parent.find("input[name='user_major']");
+	    join_center = parent.find("select[name='join_center']"),
+	    join_title= parent.find("select[name='join_title']"),
 	    $.ajax({
 	        url: '',
 	        type: 'POST',
@@ -283,9 +211,12 @@ $(document).ready(function(){
 	        	    user_sex:sex_input.val(),
 	                user_name:name_input.val(),
 	                user_phone:phone_input.val(),
+	                user_QQ:QQ_input.val(),
 	                user_college:college_input.val(),
 	                grade_input:grade_input.val(),
 	                user_major:major_input.val(),
+	                join_center:join_center.val(),
+	                join_title:join_title.val(),
 	            },
 	    });
 	 });   
